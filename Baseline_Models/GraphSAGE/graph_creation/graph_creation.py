d1c29457d49d8c13c7c5f907c54e7987db083f5e
Jseo09 <146518447+Jseo09@users.noreply.github.com>
2024-06-22 15:28:57 -0500

    Update graph_creation.py
    I have changed the code; therefore, we split the data set into 3: Testing, Validating, and Training.

--- Baseline_Models/GraphSAGE/graph_creation/graph_creation.py
+++ Baseline_Models/GraphSAGE/graph_creation/graph_creation.py
@@ -7,34 +7,35 @@ from torch_geometric.utils import from_networkx
 import networkx as nx
 from networkx.readwrite import json_graph
 import json
+import os
 
+# We first import the dataset into the code, specifically by using a pandas dataframe.
 if __name__ == '__main__':
-    # We first import the dataset into the code, specifically by using a pandas dataframe.
     df = pandas.read_csv(
-        "graph_creation/data/Darknet.CSV")
-    # Once that's done, we convert the timestamp format to something that can be better used by pandas.
-    df["Timestamp"] = pandas.to_datetime(
-        df["Timestamp"], format="%d/%m/%Y %I:%M:%S %p")
+        "/home/jseo/DGN-Graph-1/Baseline_Models/GraphSAGE/graph_creation/data/Darknet.CSV")
+     # Once that's done, we convert the timestamp format to something that can be better used by pandas.
+    df["Timestamp"] = pandas.to_datetime(df["Timestamp"], format="%d/%m/%Y %I:%M:%S %p")
     # Then create a rounded to the minute field for grouping later.
     df["timestamp_minute"] = df["Timestamp"].dt.floor("min")
-    # we sort the dataframe by timestamp earliest to latest...
+     # we sort the dataframe by timestamp earliest to latest...
     df = df.sort_values(by=["Timestamp"])
-
     # and group the entries by minute...
     grouped = df.groupby("timestamp_minute")
     # We then grab the group length for progress tracking, but this is an optional step that can be removed.
     total_groups = len(grouped)
     progress_counter = 0
-    # We then create an array for the graphs...
+ 
+     # We then create an array for the graphs...
     pyg_graphs = []
-
-    pg_json = {"graphs": []}
+    pg_json_train = {"graphs": []}
+    pg_json_val = {"graphs": []}
+    pg_json_test = {"graphs": []}
 
     # And now, for every minute in the dataset, we run the graph creation code.
     for timestamp_minute, group in grouped:
         # For testing, nodes will be named by their index, but you may want to use flow id instead.
         index = 0
-        # Then, we create the graph.
+         # Then, we create the graph.
         G = nx.DiGraph()
         # Add meta data the graph attribute.
         G.graph["timestamp"] = str(timestamp_minute)
@@ -43,23 +44,22 @@ if __name__ == '__main__':
         # We establish a previous node variable to store for easier graph creation.
         previous_backward_node = None
         # Find the number of nodes.
-        num_rows = list(range(2*len(group)))
+        num_rows = list(range(2 * len(group)))
         # Shuffle to randomly assign nodes to the training, validation, and testing sets.
         random.shuffle(num_rows)
-        training = num_rows[:int(len(num_rows)*0.7)]
-        validating = num_rows[int(len(num_rows)*0.7):int(len(num_rows)*0.85)]
-        testing = num_rows[int(len(num_rows)*0.85):]
+        
+        training = num_rows[:int(len(num_rows) * 0.7)]
+        validating = num_rows[int(len(num_rows) * 0.7):int(len(num_rows) * 0.85)]
+        testing = num_rows[int(len(num_rows) * 0.85):]
 
-        # Now, for each entry...
+         # Now, for each entry...
         for idx, row in group.iterrows():
             # Record the source and destination IP, as well as the flow ID.
             src_ip = row["Src IP"]
             dst_ip = row["Dst IP"]
-            flow_id = row[
-                "Flow ID"
-            ]  # Currently, this isn't being used, but again, you may want to use this for indexing?
+            flow_id = row["Flow ID"]
 
-            # Initialize node features dictionary with common attributes for both forward and backward nodes
+             # Initialize node features dictionary with common attributes for both forward and backward nodes
             node_features = {
                 "src_ip": src_ip,
                 "dist_ip": dst_ip,
@@ -69,7 +69,6 @@ if __name__ == '__main__':
             # Update the dictionary with specific attributes for the forward node
             # Another thing to note is that for pytorch-geo use, you need the features to share the same attributes, so the names are now shared, although the values
             # are being pulled from different places for forwards and backwards nodes.
-
             val = False
             test = False
 
@@ -77,11 +76,10 @@ if __name__ == '__main__':
                 test = True
             elif index in validating:
                 val = True
-
-            # Create the label
+            
+            # Create the label  
             fwd_label = row["Label"].lower()
-            label = 0 if (fwd_label == "nonvpn" or fwd_label ==
-                          "non-tor") else 1
+            label = 0 if (fwd_label == "nonvpn" or fwd_label == "non-tor") else 1
 
             forward_features = {"test": test, "id": index, "features": [
                 row["Total Fwd Packet"],
@@ -99,18 +97,14 @@ if __name__ == '__main__':
             ], "val": val, "label": label, "ip": src_ip, "flow_id": flow_id}
 
             node_features.update(forward_features)
-
-            # Add the forward node to the graph
             forward_node_id = index
             G.add_node(forward_node_id, **node_features)
             nodes_list.append(forward_node_id)
-
             index += 1
 
             # Create the label
             bwd_label = row["Label"].lower()
-            label = 0 if (bwd_label == "nonvpn" or bwd_label ==
-                          "non-tor") else 1
+            label = 0 if (bwd_label == "nonvpn" or bwd_label == "non-tor") else 1
 
             val = False
             test = False
@@ -133,46 +127,52 @@ if __name__ == '__main__':
                 -row["Bwd IAT Max"],
                 -row["Average Packet Size"],
                 -row["Subflow Bwd Bytes"],
-                - 1,
+                -1,
             ], "val": val, "label": label, "ip": dst_ip, "flow_id": flow_id}
 
             node_features.update(backward_features)
-
             # Add the backward node to the graph
             backward_node_id = index
             G.add_node(backward_node_id, **node_features)
             nodes_list.append(backward_node_id)
-
             index += 1
 
             # Connect the edges as appropriate (in this case, chronological order, as we are dealing with network traffic.)
             G.add_edge(forward_node_id, backward_node_id)
-            # we then connect the previous backwards node to the next forward node, although we may need to change this in the future.
             if previous_backward_node is not None:
+                 # we then connect the previous backwards node to the next forward node, although we may need to change this in the future.
                 G.add_edge(previous_backward_node, forward_node_id)
+
 
             previous_backward_node = backward_node_id
 
-            # Convert the NetworkX graph to a PyTorch Geometric Data object
-            data = from_networkx(G)
-
-            # Append the PyG Data object to the list
-            pyg_graphs.append(data)
-
-        pg_json["graphs"].append(json_graph.node_link_data(G))
+        data = from_networkx(G)
+        # Append the PyG Data object to the list
+        pyg_graphs.append(data)
         # All of the following code is optional and only exists to be able to track the progress of the program.
+        if test:
+            pg_json_test["graphs"].append(json_graph.node_link_data(G))
+        elif val:
+            pg_json_val["graphs"].append(json_graph.node_link_data(G))
+        else:
+            pg_json_train["graphs"].append(json_graph.node_link_data(G))
         # Increment progress counter
         progress_counter += 1
-
-        # Print progress as percentage
         progress_percentage = (progress_counter / total_groups) * 100
         print(f"Progress: {progress_percentage:.2f}% complete", end="\r")
 
     print("\033[K", end="\r")
     print("Processing complete.")
-    print("Converting into JSON file...")
+    print("Converting into JSON files...")
 
-    with open("graph_creation/processed_data/pygraphs.json", 'w+') as f:
-        json.dump(pg_json, f)
+    os.makedirs("/home/jseo/DGN-Graph-1/Baseline_Models/GraphSAGE/graph_creation/processed_data", exist_ok=True)
 
-    print("Convertion complete")
+    # Separate the files for the testing and training purpose
+    with open("/home/jseo/DGN-Graph-1/Baseline_Models/GraphSAGE/graph_creation/processed_data/pygraphs_train.json", 'w+') as f:
+        json.dump(pg_json_train, f)
+    with open("/home/jseo/DGN-Graph-1/Baseline_Models/GraphSAGE/graph_creation/processed_data/pygraphs_val.json", 'w+') as f:
+        json.dump(pg_json_val, f)
+    with open("/home/jseo/DGN-Graph-1/Baseline_Models/GraphSAGE/graph_creation/processed_data/pygraphs_test.json", 'w+') as f:
+        json.dump(pg_json_test, f)
+
+    print("Conversion complete")
